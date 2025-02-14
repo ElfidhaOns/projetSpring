96d00827ec2c19f1f63869d62a3a02e80e8717e0
onsfidha <onsfidha3@gmail.com>
2025-02-14 16:24:43 +0100

    chambre

---
+++ src/main/java/com/example/tp1/repository/ChambreRepository.java
@@ -0,0 +1,7 @@
+package com.example.tp1.repository;
+
+import com.example.tp1.entity.Chambre;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ChambreRepository extends JpaRepository<Chambre, Long> {
+
+}
